b24d87609aca5dc93bd94f7021c5c8f9f0d3dc7f
zvenshy <zvenshy@gmail.com>
2014-09-03 20:23:58 +0800

    增加表单提示
    js

--- m/scripts/form.js
+++ m/scripts/form.js
@@ -6,7 +6,15 @@ $(document).ready(function() {
 			var status = data.status;
 			console.log(status);
 			if (status === "success") {
+				alert("提交成功咯~");
 				location.reload();
+			} else {
+				var errorCode = data["error_code"];
+				if (error === "1") {
+					alert("你的资料填写错啦，好好检查检查唷");
+				} else {
+					alert("你已经提交过了喔");
+				}
 			}
 		}, 'json');
 	});
