44f71d44a140f178bb09e819ce09104537af35c9
Avalon <creatorlanslot@gmail.com>
2014-09-06 23:07:34 +0800

    hr内容替换

--- m/scripts/form.js
+++ m/scripts/form.js
@@ -1,20 +1,13 @@
 $(document).ready(function() {
-	$("input[type=submit]").click(function( evnet ) {
-		event.preventDefault();
+	$("form").submit(function() {
+		$("input[type=submit]").click(function(event) {
+			preventDefault();
+		});
 		url = "http://hr.ecjtu.net/index.php/register/ajax_check";
-		$.post(url, $("form").serialize(), function (data, statu) {
+		$.post(url, $(this).serialize(), function (data, statu) {
 			var status = data.status;
-			console.log(status);
 			if (status === "success") {
-				alert("提交成功咯~");
 				location.reload();
-			} else {
-				var errorCode = data["error_code"];
-				if (errorCode === "1") {
-					alert("你的资料填写错啦，好好检查检查唷");
-				} else {
-					alert("你已经提交过了喔");
-				}
 			}
 		}, 'json');
 	});
