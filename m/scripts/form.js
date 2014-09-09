231e2926980db3b02238ae1633f64e6ae5883c7d
Domaker <maker68@163.com>
2014-09-09 18:33:25 +0800

    add some text

--- m/scripts/form.js
+++ m/scripts/form.js
@@ -3,12 +3,7 @@ $(document).ready(function() {
 		$("input[type=submit]").click(function(event) {
 			preventDefault();
 		});
-		url = "http://hr.ecjtu.net/index.php/register/ajax_check";
-		$.post(url, $(this).serialize(), function (data, statu) {
-			var status = data.status;
-			if (status === "success") {
-				location.reload();
-			}
-		}, 'json');
+		url = "http://hr.ecjtu.net/index.php/register/check";
+		$.post(url, $(this).serialize());
 	});
 });
