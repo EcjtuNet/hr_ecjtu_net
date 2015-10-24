d3e2b4e8cf0546f3d348dae1421c904e043ba288
lijia <810932574@qq.com>
2015-10-24 14:54:11 +0800

    重新整合成一个页面

--- js/same.js
+++ js/same.js
@@ -1,26 +1,123 @@
-/************************通用的函数**************/
-/**获取ID toggleClass**/
-function gId(id){
-	return document.getElementById(id);
-}
-/*********************财务部和APP人员暂满***********************/
-function showRenyuanzanman(id){
-    //gId("caiwu").innerHTML.replace(/财务部/g,"人员暂满");    
-    gId(id).innerHTML="人员暂满";
-}
-function showWuwenan(id){   
-    gId(id).innerHTML="暂无文案";
-}
-function cleanRenyuanzanman1(){
-    gId("caiwu").innerHTML="财务部";
-}
-function cleanWuwenan1(){
-    gId("wuwenan").innerHTML="视觉摄影产品";
-}
-/***********display block的几个函数*************/
-function hideEffectTemplate(){
-    gId(arguments[0]).style.display = "block";
-    for(i=1;i<arguments.length;++i){
-        gId(arguments[i]).style.display = "none";
+$(document).ready(function() {
+    /*********form********/
+    $("form").submit(function() {
+        $("input[type=submit]").click(function(event) {
+            preventDefault();
+        });
+        url = "http://hr.ecjtu.net/index.php/register/check";
+        $.post(url, $(this).serialize());
+    });
+});
+/*****display BLOCK/BLUR********/
+function disBlock(){
+    for(i=0;i<arguments.length;++i){
+        $(arguments[i]).toggleClass("none");
     }
 }
+function changeBlur(){
+    for(i=0;i<arguments.length;++i){
+        $(arguments[i]).toggleClass("blur");
+    }
+}
+function templateBlock(){
+    for(i=0;i<arguments.length;++i){
+        $(arguments[i]).css("display","block");
+    }
+}
+/******effect的Animation*****/
+//$('.main').onepage_scroll();
+$(document).ready(function(){
+    $("#section1 #btn").on("click",function(){
+        $("#section1 .btn1").addClass('magictime swashIn');
+        setTimeout(function(){
+            $("#section1 .btn0").addClass('magictime spaceInRight');
+            $("#section1 .btn3").addClass('magictime spaceInLeft');
+        },800);
+        setTimeout(function(){
+            $('#section1 .btn0,#section1 .btn3').css("opacity",1);
+        },800);
+        $("#section1 #effect4").addClass('magictime slideLeftRetourn');
+        $("#section1 #effect2").addClass('magictime slideRightRetourn');
+    });
+
+    $("#section2 #btn").on("click",function(){
+        $('#section2 .btn1').addClass('magictime swashIn');
+        setTimeout(function(){
+            $('#section2 .btn0').addClass('magictime spaceInRight');
+            $('#section2 .btn3').addClass('magictime spaceInLeft');
+        },800);
+        setTimeout(function(){
+            $("#section2 .btn0,#section2 .btn3").css("opacity",1);
+        },800);
+        $('#section2 #effect1,#section2 #effect3').addClass('magictime slideUpRetourn');
+    });
+    $("#section3 #btn").on("click",function(){
+        $('#section3 .btn1').addClass('magictime swashIn');
+        setTimeout(function(){
+            $('#section3 .btn3,#section3 .btn4,#section3 .btn5').addClass('magictime swashIn');
+        },900);
+        setTimeout(function(){
+            $("#section3 .btn3,#section3 .btn4,#section3 .btn5").css("opacity",1);
+        },900);
+        $('#section3 #effect5,#section3 #effect6').addClass('magictime slideDownRetourn');
+    });
+    $("#section4 #btn").on("click",function(){
+        $('#section4 .btn1').addClass('magictime swashIn');
+        setTimeout(function(){
+            $('#section4 .btn0').addClass('magictime spaceInRight');
+            $('#section4 .btn3').addClass('magictime spaceInLeft');
+        },800);
+        setTimeout(function(){
+            $("#section4 .btn0,#section4 .btn3").css("opacity",1);
+        },800);
+        $('#section4 #effect7').addClass('magictime slideRightRetourn');
+        $('#section4 #effect8,#section4 #effect10').addClass('magictime slideUpRetourn');
+        $('#section4 #effect9').addClass('magictime slideLeftRetourn');
+    });
+});
+/*******bg与鼠标之间的移动差********/
+var lastX = 0,lastY = 0;
+function mousePosition(bg,e) {
+    //兼容IE
+    e = e ? e : window.event;
+    if (e.offsetX) {
+        x = e.offsetX;
+        y = e.offsetY;
+    }
+    //兼容ff
+    else if (e.layerX) {
+        x = e.layerX;
+        y = e.layerY;
+    }
+    var oldLeft = parseInt($(bg).css("left"));
+    var oldBottom = parseInt($(bg).css("bottom"));
+    if (x > lastX) {//鼠标向左
+        if (oldLeft > -12) {
+            $(bg).css("left", oldLeft - 1 + "px");
+        }
+    } else if (x < lastX) {//向右
+        if (oldLeft < 12) {
+            $(bg).css("left", oldLeft + 1 + "px");
+        }
+    }
+    lastX = x;
+
+    if (y > lastY) {//向下
+        if (oldBottom < -15) {
+            $(bg).css("bottom", oldBottom + 1 + "px");
+        }
+    } else if (y < lastY) {//向上
+        if (oldBottom > -45) {
+            $(bg).css("bottom", oldBottom - 1 + "px");
+        }
+    }
+    lastY = y;
+}
+/***slider****/
+$(function(){
+        $("#container").switchPage({
+            'loop' : true,
+            'keyboard' : true,
+            'direction' : 'horizontal'
+        });
+    });
---
+++ js/test.js
@@ -0,0 +1,21 @@
+
+// $('.a',context).click(function(){
+//     trigger(this);
+// })
+
+// $(document).click(function(e){
+//     switch(ele.target){
+//         case : function(){
+
+//         }
+//     }
+// })
+
+
+// var trigger = function(ele){
+//     if(ele.style == block){
+//         ele.style = none;
+//     }else{
+//         ele.style = block;
+//     }
+// }
