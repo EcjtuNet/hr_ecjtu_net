7ecc3f2a3c78f385ebf8dfd6c203c52dcc9659fd
lijia <810932574@qq.com>
2016-04-25 00:23:22 +0800

    改成竖直滑动 改了join

--- js/same.js
+++ js/same.js
@@ -27,52 +27,93 @@ function templateBlock(){
 /******effect的Animation*****/
 //$('.main').onepage_scroll();
 $(document).ready(function(){
-    $("#section1 .btn").on("click",function(){
-        $("#section1 .btn1").addClass('magictime swashIn');
+    var section1_btn = $("#section1 .btn"),
+        section1_btn0 = $("#section1 .btn0"),
+        section1_btn1 = $("#section1 .btn1"),
+        section1_btn3 = $("#section1 .btn3"),
+        section1_effect4 = $("#section1 #effect4"),
+        section1_effect2 = $("#section1 #effect2"),
+        section2_btn = $("#section2 .btn"),
+        section2_btn0 = $("#section2 .btn0"),
+        section2_btn1 = $("#section2 .btn1"),
+        section2_btn3 = $("#section2 .btn3"),
+        section2_effect1 = $("#section2 #effect1"),
+        section2_effect3 = $("#section2 #effect3"),
+        section3_btn = $("#section3 .btn"),
+        section3_btn1 = $("#section3 .btn1"),
+        section3_btn3 = $("#section3 .btn3"),
+        section3_btn4 = $("#section3 .btn4"),
+        section3_btn5 = $("#section3 .btn5"),
+        section3_effect5 = $("#section3 #effect5"),
+        section3_effect6 = $("#section3 #effect6"),
+        section4_btn = $("#section4 .btn"),
+        section4_btn0 = $("#section4 .btn0"),
+        section4_btn1 = $("#section4 .btn1"),
+        section4_btn3 = $("#section4 .btn3"),
+        section4_effect7 = $("#section4 #effect7"),
+        section4_effect8 = $("#section4 #effect8"),
+        section4_effect9 = $("#section4 #effect9"),
+        section4_effect10 = $("#section4 #effect10");
+
+    section1_btn.on("click",function(){
+        section1_btn1.addClass('magictime swashIn');
         setTimeout(function(){
-            $("#section1 .btn0").addClass('magictime spaceInRight');
-            $("#section1 .btn3").addClass('magictime spaceInLeft');
+            section1_btn0.addClass('magictime spaceInRight');
+            section1_btn3.addClass('magictime spaceInLeft');
         },800);
         setTimeout(function(){
-            $('#section1 .btn0,#section1 .btn3').css("opacity",1);
+            var arr = [section1_btn0,section1_btn3];
+            section1_btn0.css("opacity",1);
+            section1_btn3.css("opacity",1);
         },800);
-        $("#section1 #effect4").addClass('magictime slideLeftRetourn');
-        $("#section1 #effect2").addClass('magictime slideRightRetourn');
+        section1_effect4.addClass('magictime slideLeftRetourn');
+        section1_effect2.addClass('magictime slideRightRetourn');
     });
 
-    $("#section2 .btn").on("click",function(){
-        $('#section2 .btn1').addClass('magictime swashIn');
+    section2_btn.on("click",function(){
+        section2_btn1.addClass('magictime swashIn');
         setTimeout(function(){
-            $('#section2 .btn0').addClass('magictime spaceInRight');
-            $('#section2 .btn3').addClass('magictime spaceInLeft');
+            section2_btn0.addClass('magictime spaceInRight');
+            section2_btn3.addClass('magictime spaceInLeft');
         },800);
         setTimeout(function(){
-            $("#section2 .btn0,#section2 .btn3").css("opacity",1);
+            section2_btn0.css("opacity",1);
+            section2_btn3.css("opacity",1);
         },800);
-        $('#section2 #effect1,#section2 #effect3').addClass('magictime slideUpRetourn');
+        section2_effect1.addClass('magictime slideUpRetourn');
+        section2_effect3.addClass('magictime slideUpRetourn');
     });
-    $("#section3 .btn").on("click",function(){
-        $('#section3 .btn1').addClass('magictime swashIn');
+    section3_btn.on("click",function(){
+        section3_btn1.addClass('magictime swashIn');
         setTimeout(function(){
-            $('#section3 .btn3,#section3 .btn4,#section3 .btn5').addClass('magictime swashIn');
+            var arr = [section3_btn3,section3_btn4,section3_btn5];
+            $.each(arr,function(){
+                this.addClass('magictime swashIn');
+            })
         },900);
         setTimeout(function(){
-            $("#section3 .btn3,#section3 .btn4,#section3 .btn5").css("opacity",1);
+            var arr = [section3_btn3,section3_btn4,section3_btn5];
+            $.each(arr,function(){
+                this.css("opacity",1);
+            })
         },900);
-        $('#section3 #effect5,#section3 #effect6').addClass('magictime slideDownRetourn');
+        section3_effect5.addClass('magictime slideDownRetourn');
+        section3_effect6.addClass('magictime slideDownRetourn');
     });
-    $("#section4 .btn").on("click",function(){
-        $('#section4 .btn1').addClass('magictime swashIn');
+    section4_btn.on("click",function(){
+        section4_btn1.addClass('magictime swashIn');
         setTimeout(function(){
-            $('#section4 .btn0').addClass('magictime spaceInRight');
-            $('#section4 .btn3').addClass('magictime spaceInLeft');
+            section4_btn0.addClass('magictime spaceInRight');
+            section4_btn3.addClass('magictime spaceInLeft');
         },800);
         setTimeout(function(){
-            $("#section4 .btn0,#section4 .btn3").css("opacity",1);
+            section4_btn0.css("opacity",1);
+            section4_btn3.css("opacity",1);
         },800);
-        $('#section4 #effect7').addClass('magictime slideRightRetourn');
-        $('#section4 #effect8,#section4 #effect10').addClass('magictime slideUpRetourn');
-        $('#section4 #effect9').addClass('magictime slideLeftRetourn');
+        section4_effect7.addClass('magictime slideRightRetourn');
+        section4_effect8.addClass('magictime slideUpRetourn');
+        section4_effect10.addClass('magictime slideUpRetourn');
+        section4_effect9.addClass('magictime slideLeftRetourn');
     });
 });
 /*******bg与鼠标之间的移动差********/
@@ -118,6 +159,6 @@ $(function(){
         $("#container").switchPage({
             'loop' : true,
             'keyboard' : true,
-            'direction' : 'horizontal'
+            'direction' : 'vertical'
         });
     });
