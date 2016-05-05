32ecda33dba5f17fe4b3d529acd5ad5301b6588f
lijia <810932574@qq.com>
2016-05-05 23:46:51 +0800

    修改表单中部门方向不能点击选值的问题

--- js/linkage.js
+++ js/linkage.js
@@ -1,37 +1,32 @@
-$(document).ready(function () {
-    var apartmentList = {};
-    apartmentList['新闻出版中心'] = ['编辑部', '记者团'];
-    apartmentList['技术研发中心'] = ['UED', 'DOD'];
-    apartmentList['行政管理中心'] = ['办公室', '外联部'];
-    apartmentList['产品运营中心'] = ['日新微博', '日新影视工作室', '视觉摄影产品', '日新青梅竹马', '《交大青年》杂志社'];
-    function setCentre() {
-        var sc = $(".selCentre");
-        for (var i in apartmentList) {
-            //console.log(apartmentList);
-            $.each(sc, function () {
-                console.log("do it");
-                this.add(new Option(i, i));
-            });
+$(document).ready(function() {
+    var apartmentList = new Array();
+    apartmentList['新闻出版中心'] = ['记者团','编辑部'];
+    apartmentList['技术研发中心'] = ['DOD','UED'];
+    apartmentList['行政管理中心'] = ['办公室','外联部'];
+    apartmentList['产品运营中心'] = ['《交大青年》杂志社', '日新手机客户端',  '日新微博','日新影视工作室','视觉摄影产品'];
+        function setCentre(){
+            var sc = document.getElementById("selCentre");
+            for(var i in apartmentList){
+                //console.log(apartmentList);
+                sc.add(new Option(i,i));
+            }
         }
-    }
-
-    function setApart(centre) {
-        var apart = document.getElementById("selApart");
-        apart.options.length = 0;
-        //console.log(apart.options.length);
-        for (var i in apartmentList) {
-            if (i == centre) {
-                for (var j in apartmentList[i]) {
-                    //console.log(apartmentList[i]);
-                    apart.add(new Option(apartmentList[i][j], apartmentList[i][j]));
+        function setApart(centre){
+            var apart = document.getElementById("selApart");
+            apart.options.length = 0;
+            //console.log(apart.options.length);
+            for(var i in apartmentList){
+                if(i == centre){
+                    for(var j in apartmentList[i]){
+                        //console.log(apartmentList[i]);
+                        apart.add(new Option(apartmentList[i][j], apartmentList[i][j]));
+                    }
                 }
             }
         }
-    }
-
-    $("#selCentre").bind('change', function () {
-        var centre = document.getElementById("selCentre").value;
-        setApart(centre);
-    });
-    addEventListener('load', setCentre, false);
+        $("#selCentre").bind('change', function() {
+            var centre=document.getElementById("selCentre").value;
+            setApart(centre);
+        });
+        addEventListener('load',setCentre());
 });
---
+++ js/linkage_pc.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    var apartmentList = {};
+    apartmentList['新闻出版中心'] = ['编辑部', '记者团'];
+    apartmentList['技术研发中心'] = ['UED', 'DOD'];
+    apartmentList['行政管理中心'] = ['办公室', '外联部'];
+    apartmentList['产品运营中心'] = ['日新微博', '日新影视工作室', '视觉摄影产品', '日新青梅竹马', '《交大青年》杂志社'];
+    function setCentre() {
+        //中心选项
+        var sc = $(".selCentre");
+        for (var i in apartmentList) {
+            $.each(sc, function () {
+                this.add(new Option(i, i));
+            });
+        }
+
+    }
+
+    $(".selCentre").bind('change', function () {
+        var that = $(this);
+        var centre = that.val();
+        function setApart(centre) {
+            //部门方向选项
+            var apart = that.parent().parent().parent().find(".selApart");
+            //apart.length = 0;
+            for (var i in apartmentList) {
+                if (i == centre) {
+                    for (var j in apartmentList[i]) {
+                        $.each(apart,function(){
+                            this.add(new Option(apartmentList[i][j], apartmentList[i][j]));
+                        })
+                    }
+                }
+            }
+        }
+        //中心选项的值
+        setApart(centre);
+
+    });
+    addEventListener('load', setCentre, false);
+});
